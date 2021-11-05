723df2e40001897dab03d0b9917fbad1a81b1d88
0genblik <92367373+0genblik@users.noreply.github.com>
2021-11-05 15:52:04 +0000

    Final Changes
    Completed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
 	$('.header').height($(window).height());
 })
+
+/* Used to fit background image on home screen to window size,
+taken from https://websitesetup.org/bootstrap-tutorial-for-beginners/  */
